8678c1e309206210041b6f2dceb63cf3ba90901e
Hanbyul Kim <hanstar07@gmail.com>
2022-02-08 17:00:00 +0900

    Front JS 수정중

--- public/javascripts/calc.js
+++ public/javascripts/calc.js
@@ -1,1 +1,46 @@
-const formulaBox = document.getElementById(formulaBox);
+// const buttons = document.querySelectorAll('button');
+// const formulaBox = document.getElementById('formulaBox');
+// const formulaBox = document.querySelector('input');
+// const formulaBox2 = document.getElementsByTagName('button');
+
+// buttons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     formulaBox.value = button.value
+//   })});
+
+// formulaBox.addEventListener('change', () => {
+//   console.log(formulaBox)
+// });
+
+
+window.onload = function(){
+  const buttons = document.querySelectorAll('button');
+  const formulaBox = document.getElementById('formulaBox');
+
+  formulaBox.addEventListener('input', () => {
+    console.log(formulaBox.value)
+  });
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      if(button.className.includes('func1')){
+        formulaBox.value = formulaBox.value + button.value + '[]'
+      }
+      else if (button.className.includes('func2')) {
+        formulaBox.value = formulaBox.value + button.value + '[,]'
+      }
+      else if (button.className.includes('func3')) {
+        formulaBox.value = formulaBox.value + button.value + '[,,]'
+      }
+      // switch(button.value){
+      //   case 'ABS':
+      //     formulaBox.value += button.value
+      //     break
+      //
+      // }
+
+      console.log(button.className)
+    })
+    }
+  )
+}
